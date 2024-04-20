5c58a1f584e20e039cd49836e5f87fa6e1429e90
Jaswant Kumar <61474478+jaswantk917@users.noreply.github.com>
2024-04-21 00:32:03 +0530

    Update urls.py

--- message_app/urls.py
+++ message_app/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .views import submit_message, get_all_messages
 
 urlpatterns = [
+    path('/', submit_message, name='hello_world'),
     path('submit-message/', submit_message, name='submit_message'),
     path('get-all-messages/', get_all_messages, name='get_all_messages'),
 
-    
+
 ]
